720e896ff5ccbd5a35e715f879aacad125055b4d
Robert Alfaro <ralfaro@nacaplaca.com>
2026-03-05 17:27:11 -0800

    Fix js vulnerabilities detected by dependabot

--- genius-lyrics-card/rollup.config.mjs
+++ genius-lyrics-card/rollup.config.mjs
@@ -1,6 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
-import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "src/genius-lyrics-card.ts",
